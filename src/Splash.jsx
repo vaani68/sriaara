d85a3b835488ebda04280e437a5755cc073c0e6d
Vaani <71337002+vaani68@users.noreply.github.com>
2025-07-07 23:08:35 +0530

    Update Splash.jsx
    sizing

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Splash() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-white">
+    <div className="h-screen w-screen flex items-center justify-center bg-white p-4">
       <img
         src="/logo.png"
         alt="Sriaara Logo"
-        className="w-full h-full object-contain p-4"
+        className="max-h-[60vh] max-w-[80vw] object-contain"
       />
     </div>
   );
