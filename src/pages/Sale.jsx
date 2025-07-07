c83b03209b53954c08531fb55a5a1ee8c0ff0637
Vaani <71337002+vaani68@users.noreply.github.com>
2025-07-07 21:02:51 +0530

    Create Sale.jsx

---
+++ src/pages/Sale.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sale = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold">Sale Items</h2>
+      <p>Sale products will be listed here soon...</p>
+    </div>
+  );
+};
+
+export default Sale;
