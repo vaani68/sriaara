e3e0145d0dc296b8fec4ec27b1b82cc315a22589
Vaani <71337002+vaani68@users.noreply.github.com>
2025-07-07 21:01:07 +0530

    Create Splash.jsx

---
+++ src/Splash.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Splash() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-white">
+      <img src="/logo.png" alt="Sriaara Logo" className="w-40 h-40 animate-fade-in" />
+    </div>
+  );
+}
+
+export default Splash;
