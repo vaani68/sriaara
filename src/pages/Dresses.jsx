d532a5c1a76650228b38684ded18edc62456399d
Vaani <71337002+vaani68@users.noreply.github.com>
2025-07-07 21:02:23 +0530

    Create Dresses.jsx

---
+++ src/pages/Dresses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dresses = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold">Dress Material</h2>
+      <p>Dress materials will load here from CMS...</p>
+    </div>
+  );
+};
+
+export default Dresses;
