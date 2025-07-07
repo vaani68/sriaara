04c81a79213cf913f60784f027025b163fa8c1d0
Vaani <71337002+vaani68@users.noreply.github.com>
2025-07-07 23:01:22 +0530

    Update Splash.jsx
    testing tailwind

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Splash() {
   return (
     <div className="h-screen flex items-center justify-center bg-white">
-      <img src="/logo.png" alt="Sriaara Logo" className="w-40 h-40 animate-fade-in" />
+      <img src="/logo.png" alt="Sriaara Logo" className="w-40 h-40" />
     </div>
   );
 }
