5b1b9704dccbb24c04ee9bf49b546a0ffa36799c
Vaani <71337002+vaani68@users.noreply.github.com>
2025-07-09 09:22:04 +0530

    Update vite.config.js
    changing to handle proper export from .md file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Custom plugin to allow raw import of .md files
+const markdownRawLoader = {
+  name: 'markdown-raw-loader',
+  transform(src, id) {
+    if (id.endsWith('.md')) {
+      return `export default ${JSON.stringify(src)}`
+    }
+  }
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), markdownRawLoader]
 });
